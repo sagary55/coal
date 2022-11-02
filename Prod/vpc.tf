57774db30e8dd206bf4b50ffb38d57eef9fee514
sagary55 <sagary55@gmail.com>
2022-11-02 09:26:27 +0530

    Update vpc.tf

--- Prod/vpc.tf
+++ Prod/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "hitachi_vpc" {
+/*resource "aws_vpc" "hitachi_vpc" {
   cidr_block           = "${var.cidr_vpc}"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -91,4 +91,4 @@ resource "aws_route_table_association" "rta_subnet_association_pubb" {
   depends_on = [aws_route_table.rtb_public]
 }
 
-
+*/
