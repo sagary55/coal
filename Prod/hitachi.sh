994bdc76196311a44db38e29e171c0916e91501e
sagary55 <sagary55@gmail.com>
2022-11-02 11:56:05 +0530

    Update hitachi.sh

--- Prod/hitachi.sh
+++ Prod/hitachi.sh
@@ -6,7 +6,8 @@
               systemctl start httpd
               systemctl enable httpd
               echo 'Hello World This is prod' >> /var/www/html/index.html
-              sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              #sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
               #sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
               systemctl restart httpd
               systemctl restart sshd
+              
