94bf9569736cef77b5da3ed5e6b90ace5674cb0e
sagary55 <sagary55@gmail.com>
2022-11-16 17:22:30 +0530

    Update autoscaling.tf

--- Prod/autoscaling.tf
+++ Prod/autoscaling.tf
@@ -21,9 +21,9 @@ resource "aws_launch_template" "Hitachi-PROD" {
 }
 resource "aws_autoscaling_group" "Hitachi-PROD" {
   name = "Hitachi-PROD-ASG"
-  min_size             = 2
-  desired_capacity     = 2
-  max_size             = 2
+  min_size             = 3
+  desired_capacity     = 3
+  max_size             = 4
 
   health_check_type    = "EC2"
 
